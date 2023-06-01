fcb4e6e04da787b49cc7e34e13b6679cb8e58e76
illumination-k <illumination.k.27@gmail.com>
2023-06-01 19:16:33 +0900

    fix twosample

--- src/easy_plot/twosample.py
+++ src/easy_plot/twosample.py
@@ -16,7 +16,8 @@ def box_swarm_plot(
     test: Optional[Literal["t-test_welch"]] = "t-test_welch",
     text_format: Literal["simple", "star"] = "simple",
 ):
-    assert len(df[x].unique()) == 2
+    groups = list(df[x].unique())
+    assert len(groups) == 2
 
     sns.swarmplot(x=x, y=y, data=df, alpha=0.7, ax=ax)
     sns.boxplot(x=x, y=y, data=df, ax=ax, boxprops=dict(alpha=0.3))
@@ -27,7 +28,8 @@ def box_swarm_plot(
         ax.set_ylabel(ylabel)
 
     if test is not None:
-        pairs = [(x, y)]
+        
+        pairs = [(groups[0], groups[1])]
         annotator = Annotator(ax, pairs, data=df, x=x, y=y)
         annotator.configure(test="t-test_welch", text_format=text_format)
         annotator.apply_and_annotate()
